b8e50e8b651821a2ac8e4c6b7d6bde8ce37b9196
Thorsten <SolarisLunatic@gmx.de>
2020-09-19 17:17:45 +0200

    Add files via upload

---
+++ build/admin-sdk.js
@@ -0,0 +1,8 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./kiosk-online-firebase-adminsdk-yuf74-2609994c90");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://kiosk-online.firebaseio.com"
+});
---
+++ build/dist/init-firebase.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBstU_YbzbaCwLoJ6LWbzS_e0uMF3pdLqw",
+  authDomain: "kiosk-online.firebaseapp.com",
+  databaseURL: "https://kiosk-online.firebaseio.com",
+  projectId: "kiosk-online",
+  storageBucket: "kiosk-online.appspot.com",
+  messagingSenderId: "145322682878",
+  appId: "1:145322682878:web:de26cbd2e3a3bf5a4c78af",
+  measurementId: "G-WM6RF9V352"
+}; // Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
